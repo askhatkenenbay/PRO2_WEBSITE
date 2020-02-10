b67b662d3becbcf4f386baba7b77f3ced278d3d5
Askhat Kenenbay <Askhat_Kenenbay@epam.com>
2020-02-10 17:33:49 +0500

    finished

--- main/models.py
+++ main/models.py
@@ -25,6 +25,15 @@ class Person(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
 class CourseTaken(models.Model):
     user_email = models.CharField(max_length=100)
